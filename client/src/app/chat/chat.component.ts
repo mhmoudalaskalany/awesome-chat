bf5e8580610c46686741f3c179ea69aaa2438d87
mahmoud alaskalany <mhmoudalakaslanay@gmail.com>
2022-03-21 13:47:40 +0400

    add chat component

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Action } from './shared/model/action';
+import { Event } from './shared/model/event';
+import { Message } from './shared/model/message';
+import { User } from './shared/model/user';
+import { SocketService } from './shared/services/socket .service';
+
+
 @Component({
-  selector: 'app-chat',
+  selector: 'tcc-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  action = Action;
+  user: User;
+  messages: Message[] = [];
+  messageContent: string;
+  ioConnection: any;
 
-  constructor() { }
+  constructor(private socketService: SocketService) { }
 
   ngOnInit(): void {
+    this.initIoConnection();
   }
 
+  private initIoConnection(): void {
+    this.socketService.initSocket();
+
+    this.ioConnection = this.socketService.onMessage()
+      .subscribe((message: Message) => {
+        this.messages.push(message);
+      });
+
+    this.socketService.onEvent(Event.CONNECT)
+      .subscribe(() => {
+        console.log('connected');
+      });
+
+    this.socketService.onEvent(Event.DISCONNECT)
+      .subscribe(() => {
+        console.log('disconnected');
+      });
+  }
+
+  public sendMessage(message: string): void {
+    if (!message) {
+      return;
+    }
+
+    this.socketService.send({
+      from: this.user,
+      content: message
+    });
+    this.messageContent = null;
+  }
+
+  public sendNotification(params: any, action: Action): void {
+    let message: Message;
+
+    if (action === Action.JOINED) {
+      message = {
+        from: this.user,
+        action: action
+      }
+    } else if (action === Action.RENAME) {
+      message = {
+        action: action,
+        content: {
+          username: this.user.name,
+          previousUsername: params.previousUsername
+        }
+      };
+    }
+
+    this.socketService.send(message);
+  }
 }
--- client/src/app/chat/shared/model/user.ts
+++ client/src/app/chat/shared/model/user.ts
@@ -1,2 +1,3 @@
 export class User {
+    name: string
 }
